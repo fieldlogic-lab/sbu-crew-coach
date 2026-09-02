42f8b74fe819b2adbc0deea56d914326737a0769
fieldlogic-lab <timothyadammassi@gmail.com>
2026-09-02 15:37:15 -0400

    Refresh athlete assets through the service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='sbu-crew-coach-v5';
+const CACHE='sbu-crew-coach-v6';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
-  const liveAsset=/\/(app|sw|index|console)\.(js|html|css)$/.test(new URL(event.request.url).pathname);
+  const liveAsset=/\/(app|sw|index|console|athlete)\.(js|html|css)$/.test(new URL(event.request.url).pathname);
   event.respondWith(liveAsset
     ? fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))
     : caches.match(event.request).then(response=>response||fetch(event.request).then(network=>{const copy=network.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return network})));
