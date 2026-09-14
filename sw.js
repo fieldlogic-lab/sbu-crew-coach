ef95bdbab2added9af5637a8fe45dccef5a693c3
fieldlogic-lab <timothyadammassi@gmail.com>
2026-09-14 05:16:00 -0400

    Refresh coach app shell cache

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE='sbu-crew-coach-v7';
+const CACHE='sbu-crew-coach-v8';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
-  const liveAsset=/\/(app|sw|index|console|athlete)\.(js|html|css)$/.test(new URL(event.request.url).pathname);
+  const pathname=new URL(event.request.url).pathname;
+  const liveAsset=pathname==='/'||/\/(app|sw|index|console|athlete)\.(js|html|css)$/.test(pathname);
   event.respondWith(liveAsset
     ? fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))
     : caches.match(event.request).then(response=>response||fetch(event.request).then(network=>{const copy=network.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return network})));
